c160bb351ba5db318fe8bad72ab3e5f8932b4bd2
broodfish <32456928+broodfish@users.noreply.github.com>
2019-11-27 01:31:26 +0800

    upload web

---
+++ js/index.js
@@ -0,0 +1,141 @@
+var data_dir = "https://raw.githubusercontent.com/broodfish/DVVA_D3/master/countryPopulationDiffP.csv";
+var width = 800, height = 600;
+var padding = { top: 10, right: 60, bottom: 60, left: 60 };
+
+var svg = d3.select(".chart")
+	.append("svg")
+  	.attr("class", "my_canvas")
+    .attr("width", width + "px")
+    .attr("height", height + "px");
+	
+var svg = d3.select(".list")
+	.append("svg")
+  	.attr("class", "color_legend")
+    .attr("width", "300px")
+    .attr("height", "450px");
+
+var countryname = ['新北市', '宜蘭縣', '桃園市', '新竹縣', '苗栗縣', '臺中市', '彰化縣', '南投縣', '雲林縣', '嘉義縣', '臺南市', '高雄市',
+           '屏東縣', '臺東縣', '花蓮縣', '澎湖縣', '基隆市', '新竹市', '臺中市', '嘉義市', '臺南市', '臺北市', '高雄市', '福建省',
+           '金門縣', '連江縣'];
+var i = 0;
+	
+d3.csv(data_dir, function(data){
+	data.forEach(function(d){
+		d.date = parseInt(d.date);
+	});
+	
+	// get the min/max value of data
+	var date_Range = d3.extent(data, function(d){ return d.date; });
+	var populationDiff_Range = d3.extent(data, function(d){ return d.populationDiff; });
+	var country_Range = d3.extent(data, function(d){ return d.country; });
+	
+	// map "date" to X-axis
+	var x_scale = d3.scaleLinear()
+		.domain([date_Range[0], date_Range[1]])
+		.range([0, width - padding.left - padding.right]);
+	
+	// map "populationDiff" to Y-axis
+	var y_scale = d3.scaleLinear()
+		.domain([-1.5, 12])
+		.range([height - padding.top - padding.bottom, 0]);
+	
+	// map "populationDiff" to color (square)
+	var color_scale = d3.scaleOrdinal()
+		.domain([country_Range[0], country_Range[1]])
+		.range(["#5E4FA2", "#3288BD", "#66C2A5", "#ABDDA4", "#E6F598", "#FFFFBF", "#FEE08B", "#FDAE61", "#F46D43", "#D53E4F", "#9E0142"]);
+	
+	// add plot canvas
+	d3.select(".my_canvas").append("g")
+		.attr("class", "new_canvas")
+		.attr("transform", 'translate(0,0)')
+		
+	// Add X axis
+	d3.select(".my_canvas").append("g")
+		.attr("class", "x-axis")
+		.attr("transform", 'translate(' + padding.left + ',' + (height - padding.bottom) + ')')
+		.call(d3.axisBottom(x_scale));
+	
+	// Add Y axis
+	d3.select(".my_canvas").append("g")
+		.attr("class", "y-axis")
+		.attr("transform", 'translate(' + padding.left + ',' + padding.top + ')')
+		.call(d3.axisLeft(y_scale));
+	
+	var linePath = d3.line()
+                .x(function(d){ return x_scale(d.date) })
+                .y(function(d){ return y_scale(d.populationDiff) });
+	
+	function filter(i){
+		return data.filter(function(item, index, array){
+			return item.country== countryname[i];
+		});
+	}
+	
+	while (i < countryname.length){
+		d3.select(".new_canvas").append('g')
+		.append('path')
+			.attr('class', 'line-path')
+			.attr('id', function(d) { return countryname[i];})
+			.attr('transform', 'translate(' + padding.left + ',' + padding.top + ')')
+			.attr('d', linePath(filter(i)))
+			.attr('fill', 'none')
+			.attr('stroke-width', 1.5)
+			.attr('stroke', function(d) { return color_scale(countryname[i]);});
+		i += 1;
+	}
+	
+	d3.select(".new_canvas").append('g')
+		.selectAll('circle')
+		.data(data)
+		.enter()
+		.append('circle')
+		.attr('r', 2.5)
+		.attr('transform', function(d){
+			return 'translate(' + (x_scale(d.date) + padding.left) + ',' + (y_scale(d.populationDiff) + padding.top) + ')'
+		})
+		.attr('class', 'dot')
+		.attr('id', function(d) { return d.country;})
+		.style("fill", function(d) { return color_scale(d.country);});
+		
+	showTips = function(i, d){ //這部分是要作為Hover用的資料
+		var html = '' 
+		//將資料定義，並回傳
+		date = d.date;
+		country = d.country;
+		pop = d.populationDiff;
+		html = html + '<div><span>縣市：'+country+'</span><br><span>時間：'+date+'年</span><br><span>人口差：'+pop+'%</span></div>'
+		return html; //回傳整理好的html
+	};
+	
+	d3.selectAll('.dot').on('mouseover', function(d){ 
+	//d3 function，類似jquery，直接可以控制class
+		var x = d3.select(this).attr('transform').split("(")[1].split(",")[0];
+		var y = d3.select(this).attr('transform').split("(")[1].split(",")[1].split(")")[0];
+		var xPos = parseFloat(x) + 10; //截取點的位置
+		var yPos = parseFloat(y) - 10;
+		d3.select('#tooltip').style('left',xPos+'px')
+			.style('top',yPos+'px'); //將div抓來用
+		d3.select('#tooltip')
+			.classed('hidden', false) //移除隱藏的class
+			.html(showTips(i, d)); //將剛剛的showTips function帶入
+	}).on('mouseout', function(d){ //如果移出的話
+		d3.select('#tooltip').classed('hidden', true); //補回剛剛的Class
+	});	
+	
+	for (i = 0; i < countryname.length; i++){
+		d3.select(".color_legend")
+			.append("svg")
+			.attr('class', 'color_bar')
+			.attr('id', 'color_'+countryname[i])
+			.append("rect")
+				.attr('class', 'colorbox')
+				.attr('transform', 'translate(0,'+ i*20 + ')')
+				.style("fill", function(d) { return color_scale(countryname[i]);});
+		d3.select('#color_'+countryname[i])
+			.append("text")
+			.attr('class', 'text')
+			.attr('transform', 'translate(15,'+ (8.5+i*20) + ')')
+			.html(countryname[i]);
+	}
+
+});
